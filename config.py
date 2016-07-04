a069a440c0894fa56eaeffd43c5e2ab4708bd323
Simone Accascina <simon@accascina.me>
2016-07-05 01:05:04 +0200

    Add variables

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-DEBUG = True
-DEBUG_NO_SEND = True
+DEBUG = False
+DEBUG_NO_SEND = False
 
 database = {'host': 'localhost' if DEBUG else 'db', 'name': 'cnb'}
 scrape_timeout = 20 if DEBUG else 5 * 60  # seconds
+
+old_news_factor = 2
