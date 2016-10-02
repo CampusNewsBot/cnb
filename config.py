6cce42452ab11eb85c9b958e2d920569d43e0061
Simone Accascina <simon@accascina.me>
2016-10-02 19:31:47 +0200

    Move send limit config ti RethinkDB

--- config.py
+++ config.py
@@ -9,11 +9,7 @@ database = {'host': 'localhost' if DEBUG else 'db', 'db': 'cnb'}
 log_level = logging.DEBUG if DEBUG else logging.INFO
 log_file = 'cnb_unitn.log'
 
-scrape_timeout = 20 if DEBUG else 5 * 60  # seconds
-send_timeout = 10 if DEBUG else 1 * 60
+scrape_timeout = 300 if DEBUG else 5 * 60  # seconds
 
-# can send at most 3 messages every 5 * send_timeout seconds
-send_limit_messages = 3
-send_limit_intervals = 5
 
 old_news_factor = 2
--- telegram.py
+++ telegram.py
@@ -1,17 +1,17 @@
 import json
 import logging
 import time
-import collections
 import urllib.request
 import rethinkdb as r
 import config
+
+BOT_QUERY = r.table('bots').get('cnb')
 
 
 class Sender:
     def __init__(self):
         logging.info('Starting Telegram sender')
         self.db = r.connect(**config.database)
-        self.history = collections.deque(maxlen=config.send_limit_intervals)
 
     def repeat(self):
         while True:
@@ -20,20 +20,22 @@ class Sender:
             except Exception as e:
                 send_admin('Exception in message sender: {}'.format(e))
                 logging.error(e)
-            time.sleep(config.send_timeout)
+            bot = BOT_QUERY.run(self.db)
+            time.sleep(bot['send_interval'])
 
     def send_messages(self):
         logging.info('Sending news')
-        bot = r.table('bots').get('cnb').run(self.db)
-        if not (bot and bot['enabled']) or\
-                (config.DEBUG and config.DEBUG_NO_SEND):
+        bot = BOT_QUERY.run(self.db)
+
+        if not bot['enabled'] or config.DEBUG_NO_SEND:
             logging.info('Bot is disabled, quitting')
             return
-        messages = list(r.table('news').filter({'send_date': None}).run(self.db))
-        self.history.append(len(messages))
 
-        if sum(self.history) >= config.send_limit_messages:
-            r.table('bots').get('cnb').update({'enabled': False}).run(self.db)
+        messages = list(
+            r.table('news').filter({'send_date': None}).run(self.db))
+
+        if len(messages) >= bot['send_limit']:
+            BOT_QUERY.update({'enabled': False}).run(self.db)
             error_message =\
                 'Too many ({}) messages in send pipeline, disabling bot'\
                 .format(sum(self.history))
@@ -41,22 +43,22 @@ class Sender:
             send_admin(error_message)
             return
 
+        chats = r.table('chats').run(self.db)
         for message in messages:
-            chat = r.table('chats').get(message['chat']).run(self.db)
+            chat = [x for x in chats if x['id'] == message['chat']][0]
+
             logging.info('Sending news: %s', message['id'])
+
             if not chat and chat['enabled']:
                 logging.info('Chat disabled, not sending')
                 continue
-            bot_id = bot['bot_id']
-            chat_id = chat['chat_id']
 
             payload = json.dumps(
-                {'chat_id': '@' + chat_id,
+                {'chat_id': '@' + chat['chat_id'],
                  'text': '{text}\n-- {author}'.format(**message)})
 
-            logging.info('Sending news %s', message['id'])
             req = urllib.request.Request(
-                url=config.telegram_url.format(bot_id),
+                url=config.telegram_url.format(bot['bot_id']),
                 data=payload.encode('utf-8'),
                 headers={'Content-Type': 'application/json'})
             resp = urllib.request.urlopen(req)
@@ -64,19 +66,19 @@ class Sender:
             if resp.status == 200:
                 r.table('news').get(message['id'])\
                     .update({'send_date': r.now()}).run(self.db)
+                logging.info('Sent news: %s', message['id'])
             else:
-                logging.error('Send failed %s', message['id'])
+                logging.error('Send failed: %s', message['id'])
 
 
 def send_admin(message):
     db = r.connect(**config.database)
-    bot_id = r.table('bots').get('cnb').run(db)['bot_id']
     payload = json.dumps(
         {'text': message,
          'chat_id': r.table('chats').get('admin').run(db)['chat_id']})
 
     req = urllib.request.Request(
-        url=config.telegram_url.format(bot_id),
+        url=config.telegram_url.format(BOT_QUERY.run(db)['bot_id']),
         data=payload.encode('utf-8'),
         headers={'Content-Type': 'application/json'})
     urllib.request.urlopen(req)
